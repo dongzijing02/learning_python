e387611ba6d794bd0a2be14c7657f87a02b72ecd
zijing <dong.zijing@outlook.com>
2022-07-21 11:03:03 +0800

    add level order

--- Binary_tree/tree.py
+++ Binary_tree/tree.py
@@ -1,45 +1,3 @@
-# Tree
-# class TreeNode:
-#     def __init__(self, data):
-#         self.data = data
-#         self.children = []
-#         self.parent = None
-#     def add_child(self, child):
-#         child.parent = self
-#         self.children.append(child)
-#     def get_level(self):
-#         level = 0
-#         p = self.parent
-#         while p:
-#             level += 1
-#             p = p.parent
-#     def print_tree(self):
-#         spaces = ' ' * self.get_level()
-#         print(spaces + self.data)
-#         if len(self.children) > 0:
-#             for child in self.children:
-#                 child.print_tree()
-# def build_product_tree():
-#     root = TreeNode("Electronics")
-#     laptop = TreeNode("laptop")
-#     laptop.add_child(TreeNode("Mac"))
-#     laptop.add_child(TreeNode("surface"))
-#     cellphone = TreeNode("cellphone")
-#     cellphone.add_child(TreeNode("pixel"))
-#     cellphone.add_child(TreeNode("oppo"))
-#     tv = TreeNode("TV")
-#     tv.add_child(TreeNode("samsung"))
-#     tv.add_child(TreeNode("lg"))
-#     root.add_child(laptop)
-#     root.add_child(cellphone)
-#     root.add_child(tv)
-#     return root
-# if __name__ == '__main__':
-#     root = build_product_tree()
-#     # print(root.get_level())
-#     root.print_tree()
-#     pass
-
 class BinaryTree(object):
 
     def __init__(self, value):
@@ -54,10 +12,8 @@ class BinaryTree(object):
         if self is not None:
             li.append(self.value)
         if self.left is not None:
-            self.left.preorder_print()
             li = li + self.left.preorder_print()
         if self.right is not None:
-            self.right.preorder_print()
             li = li + self.right.preorder_print()
         return li
 
@@ -100,69 +56,90 @@ class BinaryTree(object):
         else:
             return False
 
+    def get_node_count(self):
+        l = 0
+        r = 0
+        if self is None:
+            return 0
+        if self.left is not None:
+            l = self.left.get_node_count()
+        if self.right is not None:
+            r = self.right.get_node_count()
+        return l + r + 1
 
-    # def get_node_count(self):
-    #     l = 0
-    #     r = 0
-    #     if self is None:
-    #         return 0
-    #     if self.left is not None:
-    #         l = self.left.get_node_count()
-    #     if self.right is not None:
-    #         r = self.right.get_node_count()
-    #     return l + r + 1
-    #
-    # def find(self, value_to_find):
-    #     l = False
-    #     r = False
-    #     if self is None:
-    #         return False
-    #     elif self.value == value_to_find:
-    #         return True
-    #     if self.left is not None:
-    #         l = self.left.find(value_to_find)
-    #     if self.right is not None:
-    #         r = self.right.find(value_to_find)
-    #     return l or r
-    #
-    # def get_height(self):
-    #     L = 0
-    #     R = 0
-    #     if self is None:
-    #         return 0
-    #     if self.left is not None:
-    #         L = self.left.get_height()
-    #     if self.right is not None:
-    #         R = self.right.get_height()
-    #     return max(L, R) + 1
-    #
-    # def count_leaves(self):
-    #     l = 0
-    #     r = 0
-    #     if self is None:
-    #         return 0
-    #     if self.right is None and self.left is None:
-    #         return 1
-    #     if self.left is not None:
-    #         l = self.left.count_leaves()
-    #     if self.right is not None:
-    #         r = self.right.count_leaves()
-    #     return l + r
+    def find(self, value_to_find):
+        l = False
+        r = False
+        if self is None:
+            return False
+        elif self.value == value_to_find:
+            return True
+        if self.left is not None:
+            l = self.left.find(value_to_find)
+        if self.right is not None:
+            r = self.right.find(value_to_find)
+        return l or r
+
+    def get_height(self):
+        L = 0
+        R = 0
+        if self is None:
+            return 0
+        if self.left is not None:
+            L = self.left.get_height()
+        if self.right is not None:
+            R = self.right.get_height()
+        return max(L, R) + 1
+
+    def count_leaves(self):
+        l = 0
+        r = 0
+        if self is None:
+            return 0
+        if self.right is None and self.left is None:
+            return 1
+        if self.left is not None:
+            l = self.left.count_leaves()
+        if self.right is not None:
+            r = self.right.count_leaves()
+        return l + r
+
+    def level_order_traversal(self):
+        queue = [self]
+        li = []
+        while len(queue) > 0:
+            vertex = queue.pop(0)
+            if vertex.left is not None:
+                queue.append(vertex.left)
+            if vertex.right is not None:
+                queue.append(vertex.right)
+            li.append(vertex.value)
+        return li
+
+    def level_order_traversal_2(self):
+        queue = [self]
+        li = []
+        while len(queue) > 0:
+            vertex = queue.pop(-1)
+            print(vertex)
+            if vertex.right is not None:
+                queue.append(vertex.right)
+            if vertex.left is not None:
+                queue.append(vertex.left)
+            li.append(vertex.value)
+        return li
 
 
 tree = BinaryTree(1)
 tree.left = BinaryTree(4)
-tree.right = BinaryTree(3)
-tree.left.right = BinaryTree(3)
-tree.left.left = BinaryTree(5)
-tree.right.left = BinaryTree(3)
-tree.right.right = BinaryTree(5)
-# tree.left.left = BinaryTree(7)
-# tree.left.right = BinaryTree(8)
-# tree.right.left = BinaryTree(9)
-# tree.right.right = BinaryTree(10)
-# tree.left.left.left = BinaryTree(6)
-# tree.left.right.right = BinaryTree(12)
+tree.right = BinaryTree(5)
+tree.left.left = BinaryTree(7)
+tree.left.right = BinaryTree(8)
+tree.right.left = BinaryTree(9)
+tree.right.right = BinaryTree(10)
+tree.left.left.left = BinaryTree(6)
+tree.left.right.right = BinaryTree(12)
+
 
 # tree2 = BinaryTree(1)
 # tree2.left = BinaryTree(4)
@@ -173,10 +150,11 @@ tree.right.right = BinaryTree(5)
 # tree2.right.right = BinaryTree(10)
 # tree2.left.left.left = BinaryTree(6)
 # tree2.left.right.right = BinaryTree(12)
+print(tree.level_order_traversal())
+print(tree.level_order_traversal_2())
 
 # print(tree.is_same_with(tree2))
-print(tree.is_symmetric(tree))
-
+# print(tree.is_symmetric(tree))
 # T = tree.preorder_print()
 # print(T)
 # print(tree.find(40))
@@ -200,5 +178,8 @@ print(tree.is_symmetric(tree))
 #         return l.value == r.value and is_symmetric(l.right, r.left) and is_symmetric(l.left, r.right)
 #     else:
 #         return False
+
+
+
 
 
