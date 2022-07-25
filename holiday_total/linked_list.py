b91e89c97a9a8b2602b649dd5d9fa98bc33066f5
zijing <dong.zijing@outlook.com>
2022-07-25 19:40:01 +0800

    reverse a linklist

--- holiday_total/linked_list.py
+++ holiday_total/linked_list.py
@@ -1,70 +1,82 @@
-
 class Node:
-    data = None
-    next_node = None
-
     def __init__(self, data):
         self.data = data
-
-    def __repr__(self):
-        return '<Node data: %s>' % self.data
-
-# N1 = Node(10)
-# print(N1)
+        self.next = None
 
 
 class LinkedList:
+    # Function to initialize head
     def __init__(self):
         self.head = None
 
-    def add(self, data):
-        new_node = Node(data)
-        new_node.next_node = self.head
-        self.head = new_node
+    def ReverseLinkList(self):
+        if self.head is None:
+            return None
+        while self.head is not None:
+            if self.next is None:
+                return self.head
+            if self.head.next is not None:
+                cur = self.head
+                self.head = self.head.next
+                self.head.next = self.head.next.next
+                self.head.next = cur
+        return self.head
 
-    # def ReverseLinkList(self):
+    def printList(self):  # 建立链表
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
     #
-    #     if self.head is None:
-    #         return None
-    #     while self.head is not None:
-    #         if self.next is None:
-    #             return self.head
-    #         if self.head.next is not None:
-    #             cur = self.head
-    #             self.head = self.head.next
-    #             self.head.next = self.head.next.next
-    #             self.head.next = cur
-    #             print(cur)
+    # def push(self, new_data):  # 新节点总是添加在给定链表的头部之前。新添加的节点成为链表的新头
+    #     new_node = Node(new_data)
+    #     new_node.next = self.head
+    #     self.head = new_node
     #
-    #     return self.head.data
-
-    # def print_link_list(self):
-    #     li = []
-    #     self.head.next = next
+    # # 在指定节点后插入
+    # def insert(self, prev_node, new_data):
+    #     if prev_node is None:
+    #         return
+    #     new_node = Node(new_data)
+    #     new_node.next = prev_node.next
+    #     prev_node.next = new_node
+    #
+    # # 在结尾加
+    # def append(self, new_data):
+    #     new_node = Node(new_data)
     #     if self.head is None:
-    #         return None
-    #     while self.head is not None:
-    #         li.append(self.head)
-    #         if self.head.next is None:
-    #             return li
-    #         if self.head.next is not None:
-    #             li.append(self.head.next)
-    #             self.head = self.head.next
-    #             return li
-    def print(self):
-        li = []
-        currentNode = self
-        while currentNode is not None:
-            li.append(currentNode)
-            currentNode = currentNode.next_node
-        print(li)
-        return li
+    #         self.head = new_node
+    #         return
+    #     last = self.head
+    #     while last.next:
+    #         last = last.next
+    #     last.next = new_node
+    #
+    # def deleteNode(self, key):
+    #     temp = self.head
+    #     if temp is not None:
+    #         if temp.data == key:
+    #             self.head = temp.next
+    #             temp = None
+    #             return
+    #     while temp is not None:
+    #         if temp.data == key:
+    #             break
+    #         prev = temp
+    #         temp = temp.next
+    #     if temp is None:
+    #         return
+    #     prev.next = temp.next
+    #     temp = None
 
 
-l = LinkedList()
-l.add(1)
-l.add(2)
-l.add(3)
-l.add(4)
-print(l)
-print(l.print())
+if __name__ == '__main__':
+    llist = LinkedList()
+    llist.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+    llist.head.next = second  # 建立指针使前一个指向后一个
+    second.next = third:
+
+print(llist.printList())
+print(llist.ReverseLinkList())
