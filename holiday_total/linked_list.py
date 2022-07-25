725ad376c06e2e2a93f80c755492cdc33280fd57
zijing <dong.zijing@outlook.com>
2022-07-25 12:49:11 +0800

    print a linklist

--- holiday_total/linked_list.py
+++ holiday_total/linked_list.py
@@ -14,37 +14,50 @@ class Node:
 
 
 class LinkedList:
-    def __init__(self):
+    def __init__(self, value):
         self.head = None
+        self.value = value
 
     def add(self, data):
-        # 在开头加数值
         new_node = Node(data)
         new_node.next_node = self.head
         self.head = new_node
 
-    def ReverseLinkList(self):
+    # def ReverseLinkList(self):
+    #
+    #     if self.head is None:
+    #         return None
+    #     while self.head is not None:
+    #         if self.next is None:
+    #             return self.head
+    #         if self.head.next is not None:
+    #             cur = self.head
+    #             self.head = self.head.next
+    #             self.head.next = self.head.next.next
+    #             self.head.next = cur
+    #             print(cur)
+    #
+    #     return self.head.data
 
+    def print_link_list(self):
+        li = []
+        self.head.next = next
         if self.head is None:
             return None
-
         while self.head is not None:
-            if self.next is None:
-                return self.head
-            if self.next is not None:
-                cur = self.head
-                self.head = next
-                next = next.next
-                self.head.next = self.cur
-
-        return self.head
+            li.append(self.head)
+            if self.head.next is None:
+                return li
+            if self.head.next is not None:
+                li.append(self.head.next)
+                self.head = self.head.next
+                return li
 
 
-
-
-l = LinkedList()
+l = LinkedList(0)
 l.add(1)
 l.add(2)
 l.add(3)
 l.add(4)
 print(l)
+print(l.print_link_list())
